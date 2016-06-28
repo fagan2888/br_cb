8200c401b9a631fe4e5903b393df39a6a53e4fce
Alex Damiani <alexdmail15@yahoo.com>
2016-06-27 22:16:11 -0700

    small error in last sub'

--- 2016_ig.py
+++ 2016_ig.py
@@ -6,13 +6,17 @@ def main():
 	path = 'C:\Users\Alex\Desktop\\'	# modify to own path for running
 	filename = 'csv IG issuance since Jan 2016.csv'
 
-	use_cols = ['Maturity\n(mm/dd/yyyy)', 'Issuer', 'Nation', 
+	use_cols = ['Maturity\n(mm/dd/yyyy)', 'Issuer', 'Nation', 'Issue\nDate',
 				'Primary\nExchange\nWhere\nIssuer\'s\nStock\nTrades', 'Coupon\n (%)', 
 				'Offer\nYield\nto Maturity\n (%)', 'Stan-\ndard\n &\nPoor\'s\nRating', 
 				'Interest\nPayment\nFrequency', 'Cpn\nType', 'Marketplace']
 
 	df = pd.read_csv(path + filename, index_col='Issue\nDate', usecols=use_cols,
 					parse_dates=True, na_values=['nan'])
+
+	print df.head()
+
+#def 
 
 if __name__ == '__main__':
 	main()
