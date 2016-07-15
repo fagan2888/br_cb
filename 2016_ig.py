c5cbbc9985749fbb39d2e9acedfdc0576dcc4f94
Alex Damiani <alexdmail15@yahoo.com>
2016-07-15 14:56:48 -0700

    Currency filter added

--- 2016_ig.py
+++ 2016_ig.py
@@ -8,7 +8,7 @@ def main():
 	filename1 = '2007-2016 DATA with ISIN.csv'
 	filename2 = 'domicile_dictionary.csv'
 	filename3 = 'Euro_countries_list.csv'
-	filename4 = 'currency codes.csv'
+	filename4 = 'currency codes_mod.csv'
 
 	df = pd.read_csv(path + filename1)
 
@@ -18,18 +18,17 @@ def main():
 
 	df = Convert_Column_Types(df)
 
-	dom_name_np = Convert_Dom_Codes(df, df_domicile)
+	df['Domicile'] = Convert_Dom_Codes(df, df_domicile)
 	curr_codes = Parse_Curr_Codes(df, df_curr_codes)
-	curr_names = Get_Curr_Names(df, df_curr_codes)
-	df['Domicile'] = dom_name_np
+	df['Currency'] = Get_Curr_Names(curr_codes, df_curr_codes)
 
+	df_cb_curr = Compare_Curr_Mkt(df, df_euro_list)
 
-
-	'''df_cb = Compare_Dom_Mktplc(df, df_euro_list)
+	df_cb_dom = Compare_Dom_Mktplc(df, df_euro_list)
 	#df_cb = Compare_Nation_Mktplc(df, df_euro_list)
 	#df_cb = Add_Global_Bonds(df_cb, df)
 
-	Flag_vs_Grouping(df, df_cb)'''
+	Flag_vs_Grouping(df, df_cb_curr, df_cb_dom)
 
 def Merge_Dfs(data_filenames, path):
 	frames = []
@@ -50,8 +49,32 @@ def Convert_Column_Types(df):
 
 	return df
 
-def Get_Curr_Names(df, df_curr_codes):
-	pass
+def Get_Curr_Names(curr_codes, df_curr_codes):
+	"""
+		Create a dictionary mapping of currency codes to country names from SDC
+		currency codes list. Map every currency code from dataset to country name.
+	"""
+	dict_curr = df_curr_codes.set_index('Code')['Country'].to_dict()
+
+	curr_name_arr = []
+	miss_code = []
+	for code in curr_codes:
+		# append nan values to keep same size as original df
+		if code == 'NaN':
+			curr_name_arr.append('NaN')
+		else:
+			try:
+				curr_name_arr.append(dict_curr[code])
+			except:
+				miss_code.append(code)
+				pass
+
+	# Make sure no missed country codes, otherwise print missed codes
+	assert (len(miss_code) == 0), np.unique(miss_code)
+
+	curr_name_np = np.array(curr_name_arr)
+
+	return curr_name_np
 
 def Parse_Curr_Codes(df, df_curr_codes):
 	""" 
@@ -66,8 +89,8 @@ def Parse_Curr_Codes(df, df_curr_codes):
 
 	curr_code_arr = []
 
-	new_arr = np.array([str.split(x) for x in curr_princ_arr])
-	for item in new_arr:
+	split_arr = np.array([str.split(x) for x in curr_princ_arr])
+	for item in split_arr:
 		try:
 			curr_code_arr.append(item[1])
 		except:
@@ -76,26 +99,7 @@ def Parse_Curr_Codes(df, df_curr_codes):
 	# Make sure no missed country codes
 	assert (len(curr_code_arr) == len(curr_princ_arr))
 
-
-	'''curr_name_arr = []
-	miss_code = []
-	for code in curr_code_arr:
-		# append nan values to keep same size as original df
-		if code == 'nan':
-			curr_name_arr.append('nan')
-		else:
-			try:
-				curr_name_arr.append(dict_domicile[code])
-			except:
-				miss_code.append(code)
-				pass
-
-	# Make sure no missed country codes
-	assert (len(miss_code) == 0), np.unique(miss_code)
-
-	curr_name_np = np.array(curr_name_arr)
-
-	return curr_name_np	'''
+	return curr_code_arr
 
 def Convert_Dom_Codes(df, df_domicile):
 	"""
@@ -127,6 +131,28 @@ def Convert_Dom_Codes(df, df_domicile):
 
 	return dom_name_np	
 
+def Compare_Curr_Mkt(df, df_euro_list):
+	"""
+		Compare issuance currency and marketplace. If they are different then add bond to 
+		cb_arr. Speical case if bond is part of European Union, then check if the marketplace 
+		isn't the EURO. 
+	"""
+	curr_names = [str.split(x)[0] for x in df['Currency'].values]
+	df['Curr Abbrev'] = curr_names
+
+	cb_arr = []
+
+	for index, row in df.iterrows():
+		if 	row['Curr Abbrev'].lower() not in row['Marketplace'].lower() \
+			and (row['Curr Abbrev'] not in df_euro_list['Country'].values and \
+				'euro' in row['Marketplace'].lower()) \
+			and row['Curr Abbrev'] != 'NaN':
+
+		   	#print row['Currency'].lower(), ':', row['Marketplace'].lower()
+		   	cb_arr.append(row)
+
+	return pd.DataFrame(cb_arr)
+
 def Compare_Dom_Mktplc(df, df_euro):
 	"""
 		Compare country of domicile and marketplace. If they are different then add bond to 
@@ -139,10 +165,10 @@ def Compare_Dom_Mktplc(df, df_euro):
 		# if domicile and marketplace don't match
 		# if they don't match, then if country is part of EU and marketplace isn't EU
 		# if they are nan, don't consider them
-		if 	row["Domicile"] not in row["Marketplace"] and \
-			(row['Domicile'] in df_euro['Country'].values and \
-		   	'Euro' not in row['Marketplace'] and 'EURO' not in row['Marketplace']) and \
-		   	row['Domicile'] != 'nan':
+		if 	row["Domicile"] not in row["Marketplace"] \
+			and (row['Domicile'] in df_euro['Country'].values and \
+		   		'euro' not in row['Marketplace'].lower()) \
+		    and	row['Domicile'] != 'nan':
 		   	#print row['Domicile'], ':', row['Marketplace']
 			cb_arr.append(row)
 
@@ -183,18 +209,28 @@ def Add_Global_Bonds(df_cb, df):
 
 	return df_merged
 
-def Flag_vs_Grouping(df_orig, df_cb):
-	df_for = df_orig[df_orig['Foreign Issue Flag(eg Yankee)(Y/N)'] == 'Yes']
-	df_cb_no = df_cb[df_cb['Foreign Issue Flag(eg Yankee)(Y/N)'] != 'Yes']
+def Flag_vs_Grouping(df_orig, df_cb_curr, df_cb_dom):
+	df_for = df_orig[df_orig['Foreign Issue Flag\r\n(eg Yankee)\r\n(Y/N)'] == 'Yes']
+	df_cb_curr_no = df_cb_curr[df_cb_curr['Foreign Issue Flag\r\n(eg Yankee)\r\n(Y/N)'] != 'Yes']
+	df_cb_dom_no = df_cb_dom[df_cb_dom['Foreign Issue Flag\r\n(eg Yankee)\r\n(Y/N)'] != 'Yes']
+
+	df_merge = df_cb_curr_no.merge(df_cb_dom_no, how='outer')
+	df_concat = pd.concat([df_merge, df_for])
+	df_concat.sort_index(inplace=True)
 
 	print 'Number of foreign flagged bonds: ', df_for.shape[0]
-	print 'Number of cross-border bonds found: ', df_cb.shape[0]
-	print 'Number of cross-border bonds without foreign flag: ', df_cb_no.shape[0]
-
-	df_concat = pd.concat([df_cb_no, df_for])
-	df_concat.sort_index(inplace=True)
+	print 'Number of cross-border currency without foreign flag: ', df_cb_curr_no.shape[0]
+	print 'Number of cross-border dominicile without foreign flag: ', df_cb_dom_no.shape[0]
+	print 'Total number of cross-border bonds with all filters:', df_concat.shape[0]
 
 	df_concat.to_csv('All cross-border & foreign flagged.csv')
 
 if __name__ == '__main__':
 	main()
+
+
+
+
+
+
+
