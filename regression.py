e62ad7b041c311f1702dccf1c7e5e0c49bad343c
Alex Damiani <alexdmail15@yahoo.com>
2016-09-01 20:12:46 -0700

    latest code

--- regression.py
+++ regression.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-
-path = r"C:\Users\Alex\Desktop\\br_cb_Data\Butterfly Currencies\\"
+import os
 
 def func(term, name):
 	curr_files = ['AUD.xlsx', 'CAD.xlsx', 'EUR.xlsx', 'GBP.xlsx', 'JPY.xlsx', 
@@ -29,7 +28,7 @@ def Set_Up_Regression_Vars(filename):
 			'Curve 10y'
 			'Currency'
 	"""
-	df = pd.read_csv(filename)
+	df = pd.read_csv(filename, index_col='Dates')
 
 	curr_order_arr = []
 	df_regr_arr = []
@@ -60,19 +59,68 @@ def Get_Regression_Vars(curr_order_arr, df_regr_arr):
 
 	return df_10_yr_int_rate_dict, df_10_yr_butt_rate_dict, df_10_yr_curr_rate_dict
 
+def Get_Curr_Basis_Swap(filename_arr, folder=''):
+	df_us_curr_swap_arr = []
+	for filename in filename_arr:
+		file_path = os.getcwd() + '\\' + folder + '\\' + filename
+		df_us_curr_swap_arr.append(pd.read_csv(file_path))
+
+	return df_us_curr_swap_arr
+
+def Create_Regression_Dfs(filename, curr_arr, curr_abbrev_dict, df_10_yr_int_rate_dict, 
+						  df_10_yr_butt_rate_dict, df_10_yr_curr_rate_dict, 
+						  df_us_curr_swap_arr):
+	df = pd.read_csv(filename, index_col='IssueDate')
+
+	count = 0
+	for index, row in df.iterrows():
+		if row['Currency'] in curr_abbrev_dict:
+			count += 1
+
+	print (count)
+
 if __name__ == '__main__':
 	#func('MS', 'monthly')	# MS = month start
 	#func('AS', 'yearly')	# AS = year start
 
-	filename = 'All_Butterfly_Spreads_monthly.csv'
+	filename_cleaned_data = 'All cross-border & foreign flagged v_9 nation.csv'
+	filename_euro_countries = 'Euro_countries_list.csv'
+	df = pd.read_csv(filename_cleaned_data)
+	df_euro_list = pd.read_csv(filename_euro_countries)
+
+	df.Nation in df_euro_list: df.Ntaion = 'EURO'
+
+
+	filename_butterfly = 'All_Butterfly_Spreads_monthly.csv'
+
 	# curr order: ['AUD', 'CAD', 'EUR', 'GBP', 'JPY', 'SEK', 'USD']
-	curr_order_arr, df_regr_arr	= Set_Up_Regression_Vars(filename)
+	curr_arr, df_regr_arr = Set_Up_Regression_Vars(filename_butterfly)
+	curr_abbrev_dict = {'US':'USD', 'Australia':'AUD', 'Canada':'CAD', 'EURO':'EUR', 
+						'Japanese':'JPY', 'UK':'GBP', 'Sweden':'SEK'}
+	nation_abbrev_dict = {'U.S.':'USD', 'Australia':'AUD', 'Canada':'CAD', 'EURO':'EUR', 
+						'Japanese':'JPY', 'UK':'GBP', 'Sweden':'SEK'}
 
 	df_10_yr_int_rate_dict, df_10_yr_butt_rate_dict, df_10_yr_curr_rate_dict = \
-				Get_Regression_Vars(curr_order_arr, df_regr_arr)
+				Get_Regression_Vars(curr_arr, df_regr_arr)
 
+	filename_curr_swaps_arr = ['AUD Basis Swaps Curve monthly.csv', 
+							   'CAD Basis Swaps Curve monthly.csv',
+							   'CHF Basis Swaps Curve monthly.csv',
+							   'CNY Basis Swaps Curve monthly.csv',
+							   'EUR Basis Swaps Curve monthly.csv',
+							   'GBP Basis Swaps Curve monthly.csv',
+							   'JPY Basis Swaps Curve monthly.csv',
+							   'SEK Basis Swaps Curve monthly.csv']
+	folder = 'Monthly CCY vs. USD curves'		#**** change to folder that contains curr swap files
 
+	df_us_curr_swap_arr = Get_Curr_Basis_Swap(filename_curr_swaps_arr, folder)
 
+	Create_Regression_Dfs(filename_cleaned_data, curr_arr, curr_abbrev_dict, 
+						  df_10_yr_int_rate_dict, df_10_yr_butt_rate_dict, 
+						  df_10_yr_curr_rate_dict, df_us_curr_swap_arr)
+
+	#df = pd.DataFrame(df_10_yr_int_rate_dict[curr_arr[0]])
+	#print (df)
 
 
 
