19967a114a44d201d4aa3e441f8284c1cf06ffb6
Alex Damiani <alexdmail15@yahoo.com>
2016-09-15 14:58:18 -0700

    Changed input file. Removed domicile function calls

--- main.py
+++ main.py
@@ -38,40 +38,28 @@ def Clean_And_Parse_Data(df, df_domicile, df_euro_list, df_curr_codes, df_rating
 
 	### Averaging ratings
 	# Already done in the 'Filtered' data set to save time
-	# df['Overall Rating S&P'] = dc.Average_Ratings(df, df_ratings_dict, 
-	#                                           rating_cols_df, rating_cols_dict)
+	df['Overall Rating S&P'] = dc.Average_Ratings(df, df_ratings_dict, 
+	                                           rating_cols_df, rating_cols_dict)
 
 	return df
 
 def Compare_Country_Cols(df, df_euro_list):
-	### Compare issuance currency and marketplace with DOMICILE
-	df_cb_curr_dom = dc.Compare_Curr_Dom(df, df_euro_list)
-	df_cb_dom_dom = dc.Compare_Dom_Mktplc(df, df_euro_list)
-	df_cb_dom_dom = dc.Remove_Curr_Filter_From_Mkt_Filter(df_cb_curr_dom, 
-	                                                   df_cb_dom_dom, df_euro_list)
-
-
 	### Compare issuance currency and marketplace with NATION
 	df_cb_curr_nat = dc.Compare_Curr_Nation(df, df_euro_list)
 	df_cb_dom_nat = dc.Compare_Nation_Mktplc(df, df_euro_list)
 	df_cb_dom_nat = dc.Remove_Curr_Filter_From_Mkt_Filter(df_cb_curr_nat, 
 	                                                   df_cb_dom_nat, df_euro_list)
 
-	return (df_cb_curr_dom, df_cb_dom_dom, df_cb_curr_nat, df_cb_dom_nat)
+	return (df_cb_curr_nat, df_cb_dom_nat)
 
-def Filter_CB_and_SSA(df_cb_curr_dom, df_cb_dom_dom, df_cb_curr_nat, df_cb_dom_nat, 
-					outfile_dom, outfile_nation):
-	#### Domicile
-	df_domicile = dc.Flag_vs_Grouping(df, df_cb_curr_dom, df_cb_dom_dom, 
-	                               df_euro_list, outfile_dom)
-	df_domicile_corp, df_domicile_ssa = dc.Filter_Out_SSA(df_domicile)
+def Filter_CB_and_SSA(df_cb_curr_nat, df_cb_dom_nat, outfile_dom, outfile_nation):
 
 	#### Nation
 	df_nation = dc.Flag_vs_Grouping(df, df_cb_curr_nat, df_cb_dom_nat, 
 	                             df_euro_list, outfile_nation)
 	df_nation_corp, df_nation_ssa = dc.Filter_Out_SSA(df_nation)
 
-	return df_domicile_corp, df_domicile_ssa, df_nation_corp, df_nation_ssa
+	return df_nation_corp, df_nation_ssa
 
 if __name__ == '__main__':
 	### Specify paths to read data files
@@ -79,8 +67,8 @@ if __name__ == '__main__':
 	data_path = r'C:\Users\Alex\Desktop\\br_cb\Data\\'	# modify to own path for running
 
 	### Specify file names of dictionaries to be used 
-	filename_data = 'Filtered with Nations 2007-2016.csv'  # has average ratings
-	#filename_data = '2007-2016 DATA with ISIN sample.csv' # doesn't have
+	#filename_data = 'Filtered with Nations 2007-2016.csv'  # has average ratings
+	filename_data = '2007-2016 DATA with ISIN.csv' # doesn't have
 	filename_domicile = 'domicile_dictionary.csv'
 	filename_euro_list = 'Euro_countries_list.csv'
 	filename_curr_codes = 'currency_codes_dict.csv'
@@ -97,15 +85,19 @@ if __name__ == '__main__':
 			  filename_curr_codes, filename_ratings, rating_cols_df, rating_cols_dict)
 
 	# Parsing columns
-	df = Clean_And_Parse_Data(df, df_domicile, df_euro_list, df_curr_codes, df_ratings_dict)
+	#df = Clean_And_Parse_Data(df, df_domicile, df_euro_list, df_curr_codes, df_ratings_dict)
+
+	#df.to_csv('2007-2016 data with ratings.csv')
+
+	df = pd.read_csv('2007-2016 data with ratings.csv')
 
 	# Comparing country, mktplace, and currencies
-	df_cb_curr_dom, df_cb_dom_dom, df_cb_curr_nat, df_cb_dom_nat = \
+	df_cb_curr_nat, df_cb_dom_nat = \
 		Compare_Country_Cols(df, df_euro_list)
 
 	# separating cross-border by ssa and corporate
-	df_domicile_corp, df_domicile_ssa, df_nation_corp, df_nation_ssa = \
-		Filter_CB_and_SSA(df_cb_curr_dom, df_cb_dom_dom, df_cb_curr_nat, df_cb_dom_nat, 
+	df_nation_corp, df_nation_ssa = \
+		Filter_CB_and_SSA(df_cb_curr_nat, df_cb_dom_nat, 
 					outfile_dom, outfile_nation)
 
 
